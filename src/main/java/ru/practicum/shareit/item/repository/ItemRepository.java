f57ebf6854d439178be7066c477d23a979e46dd9
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2023-07-14 00:08:24 +0300

    add-bookings

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -2,17 +2,17 @@ package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Collection<Item> findAllByOwnerId(Long userId);
+    List<Item> findAllByOwnerId(Long userId);
 
     @Query("SELECT i FROM Item AS i " +
             "WHERE (LOWER(i.name) LIKE LOWER(concat('%', :text, '%')) " +
             "OR LOWER(i.description) LIKE LOWER(concat('%', :text, '%'))) AND i.available=true")
     List<Item> search(String text);
 }
-
