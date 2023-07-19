be07e10a946af08b3a6c76054523811e9e6fc189
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2023-07-19 23:07:55 +0300

    15 Technical Specification/need tests

--- src/main/java/ru/practicum/shareit/request/exception/RequestNotFoundException.java
+++ src/main/java/ru/practicum/shareit/request/exception/RequestNotFoundException.java
@@ -1,7 +1,7 @@
 package ru.practicum.shareit.request.exception;
 
-public class RequestNotFoundException extends RuntimeException{
-    public RequestNotFoundException (String message) {
+public class RequestNotFoundException extends RuntimeException {
+    public RequestNotFoundException(String message) {
         super(message);
     }
 }
--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,18 +1,21 @@
 package ru.practicum.shareit.request.model;
 
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "requests")
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ItemRequest {
 
     @Id
@@ -23,7 +26,24 @@ public class ItemRequest {
     private String description;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "requester_id", nullable = false)
+    @ToString.Exclude
     private User requester;
     @Column(name = "created", nullable = false)
     private LocalDateTime created;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ItemRequest that = (ItemRequest) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
 }
