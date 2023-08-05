11ec54523640069ff41134559672c746951c8aaa
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2023-08-05 15:17:51 +0300

    docker правка 12 тестов

--- gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,14 +1,11 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.Builder;
-import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
-import lombok.Getter;
-import lombok.Setter;
 import ru.practicum.shareit.booking.dto.BookingDto;
 
 import java.util.List;
