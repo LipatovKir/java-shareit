f9b8240827fd96c001d5330b74e177cf20dde872
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2023-08-06 14:52:37 +0300

    Update ItemRepository.java

--- server/src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ server/src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -14,6 +14,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByRequestId(long requestId);
 
+    List<Item> findByOwnerById(long userId);
+
     @Query("select i from Item i where upper(i.name) like upper(concat('%', ?1, '%')) or upper(i.description) " +
             "like upper(concat('%', ?1, '%')) and i.available = true ")
     List<Item> search(String text, PageRequest pageRequest);
